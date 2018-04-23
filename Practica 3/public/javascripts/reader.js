54a86f8f9db99470af0be4d129f92720c05f1e3a
Andrei Ionut Vaduva <andi_vaduva@yahoo.es>
2018-04-23 13:48:51 +0200

    no s

---
+++ Practica 3/public/javascripts/reader.js
@@ -0,0 +1,50 @@
+"use strict";
+
+function MyFileReader() {
+
+    this.readData = function readData(event,attrLen,callback) {
+        let file = event.target.files[0];
+        let reader = new FileReader();
+        reader.onload = function(event) {
+            let data = event.target.result;
+            let arrayOfLines = data.split('\r\n');
+            let info = [];
+            info = dataParser(attrLen,arrayOfLines);
+            if(info.length <= 0){
+                callback(undefined);
+            }
+            else{
+                callback(null,info);
+            }
+        };
+        reader.readAsText(file);
+    };
+
+    function dataParser(attrLen,arrayOfLines){
+        let info = [];
+        for(let line of arrayOfLines){
+            let arrayOfAttr = line.split(',');
+            //If attr file have the same nr of attributes, we read
+            if(arrayOfAttr.length === attrLen){
+                info.push(arrayOfAttr);
+            }  
+        }
+        return info;
+    };
+
+    this.readAttributes = function readAttributes(event, callback) {
+        let file = event.target.files[0];
+        let reader = new FileReader();
+        reader.onload = function(event) {
+            let data = event.target.result;
+            let attributes = data.split(',');
+            if(attributes.length <= 0){
+                callback(undefined);
+            }
+            else{
+                callback(null,attributes);
+            }
+        };
+        reader.readAsText(file);
+    };
+};
